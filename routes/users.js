971634388be004ef94f1bd068d7094acd9e2b3bc
Maulik panchal <maulikcharmi1811@gmail.com>
2023-08-08 21:37:12 +0530

    css

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@ const passport=require('passport');
 const router = express.Router();
 const usersControllers = require('../controllers/users_controller')
 router.get('/profile', passport.checkAuthentication, usersControllers.profile);
-router.post('/update/:id', passport.checkAuthentication, usersControllers.update);
+router.post('/updateUserProfile', passport.checkAuthentication, usersControllers.update);
 router.get('/sign-up', usersControllers.signUp);
 router.get('/sign-in', usersControllers.signIn);
 router.post('/createUser', usersControllers.create);

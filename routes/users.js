7eeaa95c746fd05266da71079915694b0d7d9a12
Maulik panchal <maulikcharmi1811@gmail.com>
2023-08-09 17:52:22 +0530

    adding css

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const passLocal = require('passport-local');
 const passportLocalStrategy = require('../config/passport-local-strategy');
-const passport=require('passport');
+const passport = require('passport');
 // const passportLocalStrategy = require('passport-local');
 const router = express.Router();
 const usersControllers = require('../controllers/users_controller')
@@ -11,15 +11,15 @@ router.post('/updatingUserProfile', passport.checkAuthentication, usersControlle
 router.get('/sign-up', usersControllers.signUp);
 router.get('/sign-in', usersControllers.signIn);
 router.post('/createUser', usersControllers.create);
-router.post('/signup-otp-varification',usersControllers.otpVarification_For_SignUp);
-router.post('/sign-in-otpvarification',passport.authenticate(
+router.post('/signup-otp-varification', usersControllers.otpVarification_For_SignUp);
+router.post('/sign-in-otpvarification', passport.authenticate(
     'local',
-    {failureRedirect:'/users/sign-in'}, 
-),usersControllers.otpVarification_For_SignIn);
-router.post('/sign-up_resend-Otp',usersControllers.resendOtp_SignUp);
-router.post('/resend-Otp-For-UpdateEmail',usersControllers.resendOtp_For_updateEmail)
-router.post('/otp-varification',usersControllers.sending_Sign_In_OTP);
-router.post('/sign-in_resend-Otp',usersControllers.resendOtp_SignIn);
-router.post('/UpdateEmailOTP',usersControllers.updateEmailOTP);
+    { failureRedirect: '/users/sign-in' },
+), usersControllers.otpVarification_For_SignIn);
+router.post('/sign-up_resend-Otp', usersControllers.resendOtp_SignUp);
+router.post('/resend-Otp-For-UpdateEmail', usersControllers.resendOtp_For_updateEmail)
+router.post('/otp-varification', usersControllers.sending_Sign_In_OTP);
+router.post('/sign-in_resend-Otp', usersControllers.resendOtp_SignIn);
+router.post('/UpdateEmailOTP', usersControllers.updateEmailOTP);
 router.get('/sign-out', usersControllers.destroySession);
 module.exports = router;      
